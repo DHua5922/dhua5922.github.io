63d998fe002086be30a31206725fab49189c8192
Dylan Hua <hua.dylan@gmail.com>
2020-03-21 08:52:29 -0500

    Redesigned homepage

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,24 +1,22 @@
 import React, {Component} from 'react';
 import './Home.scss';
-import '../../index.scss';
-import  {PageLink} from '../page-link/PageLink';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import {NavigationBar} from '../navbar/NavigationBar';
 
 class Home extends Component {
   render() {
     return (
-      <>
-        <Container fluid className="page">
-          <Row>
-            <Col xs={12} md={6} className="pad-right pad-bot"><PageLink label="Home"/></Col>
-            <Col xs={12} md={6} className="pad-left pad-bot"><PageLink label="About"/></Col>
-            <Col xs={12} md={6} className="pad-right pad-top"><PageLink label="Portfolio"/></Col>
-            <Col xs={12} md={6} className="pad-left pad-top"><PageLink label="Contact"/></Col>
-          </Row>
-        </Container>
-      </>
+      <div className="homepage">  
+        <NavigationBar />
+        <div className="overlay">
+          <div className="home-text">
+            <h1>Dylan Hua</h1>
+            <h2>Full Stack Developer and Cloud Software Developer</h2>
+          </div>
+          <div>
+            
+          </div>
+        </div>
+      </div>
     );
   }
 }
