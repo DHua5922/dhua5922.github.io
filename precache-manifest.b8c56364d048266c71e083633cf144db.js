927cfa24d1855837b7618807a4993ed4f9b3b236
Dylan Hua <hua.dylan@gmail.com>
2020-03-24 13:40:57 -0500

    Updates

---
+++ precache-manifest.b8c56364d048266c71e083633cf144db.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44bd4c8c5cdfcacad8b2003d7ffae3a5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b17f1472d32fac733cb4",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "526186b2e7b867c8fd63",
+    "url": "/static/css/main.0e3608cd.chunk.css"
+  },
+  {
+    "revision": "b17f1472d32fac733cb4",
+    "url": "/static/js/2.dfdd1f0e.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/static/js/2.dfdd1f0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "526186b2e7b867c8fd63",
+    "url": "/static/js/main.8db3fb6c.chunk.js"
+  },
+  {
+    "revision": "6ef5fe402e534b5d4b41",
+    "url": "/static/js/runtime-main.c43d354e.js"
+  },
+  {
+    "revision": "f6bace5bc19d2116b3a197d13e7067bf",
+    "url": "/static/media/img_arsphere.f6bace5b.png"
+  },
+  {
+    "revision": "703ec32c32950e168795779a3439e296",
+    "url": "/static/media/img_cars.703ec32c.jpg"
+  },
+  {
+    "revision": "4ab7bce3f7d068a776fcf980b7cee131",
+    "url": "/static/media/img_cmovie.4ab7bce3.png"
+  },
+  {
+    "revision": "a5ef2a3922274317d8bcb1ce5be79324",
+    "url": "/static/media/img_hospital.a5ef2a39.png"
+  },
+  {
+    "revision": "1ba068f236e22b22d2b14225f7ffbbfc",
+    "url": "/static/media/img_journalsite.1ba068f2.png"
+  },
+  {
+    "revision": "7a7fbb1b835be40f50547b267494e7aa",
+    "url": "/static/media/img_me.7a7fbb1b.jpg"
+  },
+  {
+    "revision": "4eb6ca5cdd682f89c2fb5753c3fb38cb",
+    "url": "/static/media/img_morse.4eb6ca5c.jpg"
+  },
+  {
+    "revision": "a23ea6dcacd93c80234865659f4892cc",
+    "url": "/static/media/img_nutrition.a23ea6dc.jpg"
+  },
+  {
+    "revision": "ae6fe0851f9dd22992b47d40a51363c4",
+    "url": "/static/media/img_shoppinglist.ae6fe085.png"
+  },
+  {
+    "revision": "dd5a8f184e310642b945063e6135e9ef",
+    "url": "/static/media/img_switcher.dd5a8f18.png"
+  },
+  {
+    "revision": "3de1af0de7300d01c44fdb234c5944b5",
+    "url": "/static/media/video_arsphere.3de1af0d.mp4"
+  },
+  {
+    "revision": "e6194b05a3424a0682c96c0e853c4cc9",
+    "url": "/static/media/video_hospital.e6194b05.mp4"
+  },
+  {
+    "revision": "ce991132be476f0b1b203ac4f53ee56b",
+    "url": "/static/media/video_journalsite.ce991132.mp4"
+  }
+]);
