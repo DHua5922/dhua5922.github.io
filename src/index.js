f084cb4cd5726be10242f0e30a61fd60b3cf479e
DHua5922 <hua.dylan@gmail.com>
2021-04-29 09:28:49 -0500

    Refactored contact me section

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Contact} from './components/contact/Contact';
 import * as serviceWorker from './serviceWorker';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import "./index.scss";
 import Footer from './components/Footer';
-import "tailwindcss/tailwind.css";
 import Navbar from './components/Navbar';
 import About from './sections/About';
 import Portfolio from './sections/Portfolio';
+import Contact from './sections/Contact';
 
 const routing = (
     <main>
