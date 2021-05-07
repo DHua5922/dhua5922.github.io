962aaf0304876c14147143d10ee06b5e354b7dfe
Dylan Hua <hua.dylan@gmail.com>
2021-05-07 10:09:39 -0500

    add-about-summary (#39)

--- src/sections/About.js
+++ src/sections/About.js
@@ -4,22 +4,27 @@ import Section from '../components/views/Section';
 
 const Container = styled(Section)`
     padding: 40vh 10vw;
-    ${tw`bg-white text-black text-center`}
+    ${tw`bg-white text-black`}
 `;
 
 const Name = styled.h1`
-    ${tw`text-8xl mb-12`}
+    ${tw`text-7xl mb-12 text-center`}
 `;
 
 const Description = styled.h1`
-    ${tw`text-lg`}
+    max-width: 800px;
+    ${tw`text-lg m-auto`}
 `;
 
 export default function About({ ...props }) {
     return (
         <Container {...props}>
             <Name>Dylan Hua</Name>
-            <Description>Front End Developer</Description>
+            <Description>
+                I am a front end developer who loves to make beautiful applications like the slick React website that helped over 20,000 users
+                request IT services. Making a website that evaluates a stock using React and Node.js helped me gain full stack development experience. 
+                To continue honing my skills and learn new things, I take on personal projects on my free time.
+            </Description>
         </Container>
     ); 
 }
