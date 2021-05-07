f27c260df47125c37a2c748dfe04b77cd028fb21
DHua5922 <hua.dylan@gmail.com>
2021-05-07 07:17:14 -0500

    Removed footer

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,38 +2,87 @@ import { Navbar as BNavbar, Nav } from 'react-bootstrap';
 import React from "react";
 import tw, { styled } from "twin.macro";
 import { useScrollSection } from 'react-scroll-section';
+import { FaLinkedin } from "react-icons/fa";
+import { AiFillGithub } from "react-icons/ai";
 
 function useLinks() {
-    return [
-        {
-            label: "About",
-            ...useScrollSection('about')
-        },
-        {
-            label: "Portfolio",
-            ...useScrollSection('portfolio')
-        },
-        {
-            label: "Contact",
-            ...useScrollSection('contact')
-        },
-    ];
+    return {
+        left: [
+            {
+                label: "About",
+                ...useScrollSection('about')
+            },
+            {
+                label: "Portfolio",
+                ...useScrollSection('portfolio')
+            },
+            {
+                label: "Contact",
+                ...useScrollSection('contact')
+            },
+        ],
+        right: [
+            {
+                url: "https://www.linkedin.com/in/dylanhua/",
+                icon: <FaLinkedin tw="text-white text-3xl" />,
+            },
+            {
+                url: "https://github.com/DHua5922",
+                icon: <AiFillGithub tw="text-white text-3xl" />,
+            },
+        ],
+    };
 }
 
 const SNavbar = styled(BNavbar)`
     ${tw`bg-black`}
 `;
 
+function NavLink({ children, ...rest }) {
+    return (
+        <Nav.Link
+            rel="noopener noreferrer" 
+            target="_blank"
+            {...rest}
+        >
+            {children}
+        </Nav.Link>
+    );
+}
+
 export default function Navbar() {
+    const { left, right } = useLinks();
     return (
         <SNavbar collapseOnSelect expand="lg" variant="dark" sticky="top">
             <BNavbar.Toggle aria-controls="responsive-navbar-nav" />
             <BNavbar.Collapse id="responsive-navbar-nav">
-                <Nav className="ml-auto mr-auto">
+                <Nav className="mr-auto">
                     {
-                        useLinks().map(link => {
+                        left.map((link, index) => {
                             const { label, ...rest } = link;
-                            return <Nav.Link {...rest}>{label}</Nav.Link>;
+                            return (
+                                <NavLink 
+                                    key={index} 
+                                    {...rest}
+                                >
+                                    {label}
+                                </NavLink>
+                            );
+                        })
+                    }
+                </Nav>
+                <Nav className="ml-auto">
+                    {
+                        right.map((link, index) => {
+                            const { url, icon } = link;
+                            return (
+                                <NavLink 
+                                    key={index}
+                                    href={url}
+                                >
+                                    {icon}
+                                </NavLink>
+                            );
                         })
                     }
                 </Nav>
